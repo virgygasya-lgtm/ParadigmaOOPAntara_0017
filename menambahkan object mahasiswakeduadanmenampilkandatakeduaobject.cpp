413eaa93a998c940d991f5a6b40d9e9a655ff24e
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-07-30 14:23:53 +0700

    menambahkan object mahasiswakeduadanmenampilkandatakeduaobject.cpp

--- menambahkan object mahasiswakeduadanmenampilkandatakeduaobject.cpp
+++ menambahkan object mahasiswakeduadanmenampilkandatakeduaobject.cpp
@@ -20,3 +20,13 @@ int main() {
     mhs.nim = 2022;
     mhs.nama = "Abra";
     mhs.nilai = 90.5;
+
+    Mahasiswa mhs1;
+
+mhs1.nim = 2022;
+mhs1.nama = "Naya";
+mhs1.nilai = 80.5;
+
+mhs.printData();
+mhs1.printData();
+}
