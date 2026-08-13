bb5f03fe636de41576915f8300cbf50bfbc94a9d
virgygasya-lgtm <virgy.gasya@gmail.com>
2026-08-13 14:14:01 +0700

    membuatconstructororang.cpp

---
+++ membuatconstructororang.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//pembuatan class orang
+class orang {
+public:
+    //deklarasi member variable dari class orang
+    string nama;
+
+        //pembuatan constructor dan destructor dari class orang
+    orang(string pNama) :nama(pNama) {
+        cout << "Orang dibuat\n" << endl;
+    }
